c1e8d429ee98b28060882b22d7c2f8f62b46b64d
aman <amankumarmatta0@gmail.com>
2025-11-22 23:05:30 +0530

    Created Folders and Added Stow Bash

---
+++ stow.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+stow --target=$HOME/.config/fastfetch fastfetch
+stow --target=$HOME/.config/fish fish
+stow --target=$HOME/.config/hypr hypr
+stow --target=$HOME/.config/kitty kitty
+stow --target=$HOME/.config/rofi rofi
+stow --target=$HOME/.config/waybar waybar
