b1fdebb91dc87515bf6d30fbc47160f0936dd0b3
aman <amankumarmatta0@gmail.com>
2025-11-29 12:02:03 +0530

    Added Pokemon Colorscript to Fastfetch

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
 	set -U fish_greeting
-	fastfetch
+	pokemon-colorscripts --no-title -s -b -n pikachu| fastfetch -c $HOME/.config/fastfetch/config.jsonc --logo-type file-raw --logo-height 10 --logo-width 5 --logo -
 	starship init fish | source
 end
+
